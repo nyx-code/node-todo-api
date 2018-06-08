b529ca9639b6c78092866a8f5e72eef753764dae
nyx-coder <kadamshailesh007@gmail.com>
2018-06-08 14:45:40 +0530

    Added User model

--- server/server.js
+++ server/server.js
@@ -5,38 +5,58 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'Cook Dinner'
+// var newTodo = new Todo({
+//     text: 'Cook Dinner'
+// });
+
+// newTodo.save().then((res) => {
+//     console.log('Todo Saved', res);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+// var newTodo1 = new Todo({
+//     text: "      Eat Lunch         "
+// })
+
+// newTodo1.save().then((res) => {
+//     console.log('Todo Saved successfuly');
+//     console.log(JSON.stringify(res, undefined, 2));
+// }, (err) => {
+//     console.log('Unable to save data', err);
+// });
+
+var Users = mongoose.model('Users', {
+    email: {
+        required: true,
+        trim: true,
+        type: String,
+        minlength: 1
+    }
 });
 
-newTodo.save().then((res) => {
-    console.log('Todo Saved', res);
-}, (e) => {
-    console.log('Unable to save todo');
+var newUser = new Users({
+    email: 'kadamshailesh007@gmail.com'
 });
 
-var newTodo1 = new Todo({
-    text: 'Record Youtube Videos',
-    completed: true,
-    completedAt: 12
-}, {
-        text: 'task2',
-        completed: false,
-    })
-
-newTodo1.save().then((res) => {
-    console.log('Todo Saved successfuly');
+newUser.save().then((res) => {
+    console.log('New User created succfully');
     console.log(JSON.stringify(res, undefined, 2));
 }, (err) => {
-    console.log('Unable to save data', err);
+    console.log('Unable to create new user because', err);
 });
