d7ec0de68bcd691f75cc2af4a3c397d756f8cb50
nyx-coder <kadamshailesh007@gmail.com>
2018-06-19 16:13:20 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+const MONGO_URI = 'mongodb://todoapp:pass@123@ds161610.mlab.com:61610/dbtodoapp'
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var { Todo } = require('./model/todo');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
@@ -45,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
-app.listen(3000, () => {
-    console.log('Started on Port 3000');
+app.listen(port, () => {
+    console.log(`Started on Port ${port}`);
 });
 
 module.exports = { app };
